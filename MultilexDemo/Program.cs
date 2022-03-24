811300fb0262ce7243552f1e55e20d0a08e6fa91
Matt Webb <matthew.webb@fdbhealth.com>
2022-03-24 14:18:33 +0000

    Hosting, DI, Config & SQLClient Integrated

--- MultilexDemo/Program.cs
+++ MultilexDemo/Program.cs
@@ -1,17 +1,63 @@
+
+
+
+using FirstDataBank.DrugServer.API;
+using FirstDataBank.DrugServer.API.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace MultilexDemo
 {
-    internal static class Program
+    public static class Program
     {
+        public static IServiceProvider? ServiceProvider { get; set; }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+
+            CreateHostBuilder(args).Build().RunAsync();
+
+            var mainForm = ServiceProvider?.GetRequiredService<MainForm>();
+            Application.Run(mainForm);
         }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddDrugServerApi(settings =>
+                    {
+                        //Get Settings from Config File
+                        settings.ConfigurationSourceSettings = context.Configuration.GetSection("FDBDrugServerSettings");
+                    });
+                    services.AddSingleton<IDrugSystem>(serviceProvider =>
+                    {
+                        var factory = serviceProvider.GetRequiredService<IDrugSystemFactory>();
+                        return factory.CreateSystem();
+                    });
+
+                    services.AddScoped<MainForm>();
+
+                    ServiceProvider = services.BuildServiceProvider();
+                });
+        }
+
     }
 }
